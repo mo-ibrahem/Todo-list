1ce5a993676f93b80eae3e6ef6f030dd9ae35060
Mohamed Abdelaziz <mmody.zizo@Hotmail.com>
2022-02-05 11:51:34 +0000

    Adding ejs

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const bodyParser = require('body-parser');
  
 const app = express();
 app.set('view engine', 'ejs');
-
+var today = new Date();
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
 
-    if (today.getDay() == 1 || today.getDay() == 2){
-        res.send("<h1>Yay Weekend</h1>");
-    }else{
-        res.sendFile(__dirname + "/index.html");
+    var options={
+        weekday: 'long',day: 'numeric', month: 'long'
     }
+
+    var day = today.toLocaleDateString("en-US", options);
+    
+
+    res.render('list', {KindOfday: day});  
+
+
+
+
+
 })
 
 app.listen(3000, function () {
