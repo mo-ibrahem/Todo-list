121cccc22a2aa2e777383a662286e754c12bd761
Mohamed Abdelaziz <mmody.zizo@Hotmail.com>
2022-02-06 10:36:24 +0000

    added styling to our todolist and more pages

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 var today = new Date();
 var items = ["Finish 2 full stack courses", "Workout","Read for your Diseration"];
+var workItems = [];
+app.get('/about', function(req, res) {
+    res.render('about');
+})
 app.get('/', (req, res) => {
 
 
@@ -17,16 +21,33 @@ app.get('/', (req, res) => {
     var day = today.toLocaleDateString("en-US", options);
     
 
-    res.render('list', {KindOfday: day, newlistItems: items});  
+    res.render('list', {listTitle: day, newlistItems: items});  
 
 
+})
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: "Work List", newlistItems: workItems});
 })
 
 app.post('/', (req, res) => {
+    console.log(req.body);
     var item =  req.body.newItem;
+    if (req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect('/work');
+    }
+    else{
     items.push(item);
     res.redirect("/");
+    }
 })
+app.post('/work', (req, res) => {
+    var item =  req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+
 
 app.listen(3000, function () {
     console.log("listening on 3000");
